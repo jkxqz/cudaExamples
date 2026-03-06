dfa4a3d420234df9ba5ac9818536de713902fd41
jkxqz <jkxqz@protonmail.com>
2026-03-06 17:44:23 -0600

    implemented tiled convolution case

--- convolution/convolution.cu
+++ convolution/convolution.cu
@@ -1,23 +1,27 @@
-// kernels for convolving a 2D matrix with a square filter
+/* 
+ convolution.cu:
+
+ kernels for convolving a 2D matrix with a square filter.
+ convolved matrix is same size as the input matrix.
+*/
+
 #include <stdio.h>
 
 #define FILTER_RADIUS 1 // square convolution kernel of radius=1
+
+// input and output dimensions for haloed convolution kernel
+#define INDIM 16
+#define OUTDIM (INDIM-2*FILTER_RADIUS)
+
 
 // constant memory allocation for read-only copy of convolution kernel
 __constant__ float F_c[2*FILTER_RADIUS+1][2*FILTER_RADIUS+1];
 
 
-// TODO
-__global__
-void tiledHaloConvKernelT2In(float* A, float* B, int height, int width);
-
-// TODO
-__global__
-void tiledHaloConvKernelT2Out(float* A, float* B, int height, int width);
-
+// CASE 0
 __global__
 void basicConvKernel(float* A, float* B, int height, int width) {
-    // thread organization: one output thread per element in output matrix B
+    // thread organization: one thread per element in output matrix B
 
     int outCol = blockIdx.x * blockDim.x + threadIdx.x;
     int outRow = blockIdx.y * blockDim.y + threadIdx.y;
@@ -42,7 +46,53 @@ void basicConvKernel(float* A, float* B, int height, int width) {
     
 }
 
-void doConv(float* A_h, float* B_h, float* F_h, int height, int width) {
+
+// CASE 1
+__global__
+void tiledHaloConvKernelT2In(float* A, float* B, int height, int width) {
+    // thread organization: one thread per element in halo input matrix
+    
+    int outRow = blockIdx.y*OUTDIM + threadIdx.y - FILTER_RADIUS;
+    int outCol = blockIdx.x*OUTDIM + threadIdx.x - FILTER_RADIUS;
+
+
+    // allocate shared memory on GPU for storing common input values (tiling)
+    __shared__ float A_s[INDIM][INDIM];
+
+    if (outRow >= 0 && outRow < height && outCol >=0 and outCol < width) {
+        A_s[threadIdx.y][threadIdx.x] = A[outRow*width + outCol];
+    } else {
+        A_s[threadIdx.y][threadIdx.x] = 0.0; // default value
+    }
+
+    __syncthreads(); // make sure threads have written to A_s before upcoming reads
+
+    if (outRow >= 0 && outRow < height && outCol >=0 and outCol < width) {
+        int sCol = threadIdx.x - FILTER_RADIUS;
+        int sRow = threadIdx.y - FILTER_RADIUS;
+
+        if (sCol >= 0 && sCol < OUTDIM && sRow >= 0 && sRow < OUTDIM) {
+            float bValue = 0.0;
+            for (int fRow=0; fRow < 2*FILTER_RADIUS+1; ++fRow) {
+                for (int fCol=0; fCol < 2*FILTER_RADIUS+1; ++fCol) {
+                    bValue += A_s[sRow][sCol]*F_c[fRow][fCol];
+                    sCol += 1; 
+                }
+		sCol = threadIdx.x - FILTER_RADIUS;
+                sRow += 1; 
+            }
+            B[outRow*width+outCol] = bValue;
+        }
+    }
+}
+
+// TODO
+// CASE 2
+__global__
+void tiledHaloConvKernelT2Out(float* A, float* B, int height, int width);
+
+
+void doConv(float* A_h, float* B_h, float* F_h, int height, int width, int kernel_choice=0) {
     
     // num bytes in A_h (same as in B_h)
     int size = width*height*sizeof(float);
@@ -63,16 +113,35 @@ void doConv(float* A_h, float* B_h, float* F_h, int height, int width) {
     // copy values of F_h into constant GPU memory used for F_c
     cudaMemcpyToSymbol(F_c, F_h, sizeof(F_c));
 
-    // arbitrarily setting blockSize to 16
-    float blockSize = 16.0f;
+    switch(kernel_choice) {
+        case 0: {
+            // arbitrarily setting blockSize to 16
+            float blockSize = 16.0f;
 
-    dim3 dimBlock(blockSize, blockSize, 1);
-    dim3 dimGrid(ceil(width/blockSize), ceil(height/blockSize), 1);
+            dim3 dimBlock(blockSize, blockSize, 1);
+            dim3 dimGrid(ceil(width/blockSize), ceil(height/blockSize), 1);
 
-    // launch kernel to perform convolution
-    basicConvKernel<<<dimGrid, dimBlock>>>(A_d, B_d, height, width);
+            // launch kernel to perform convolution
+            basicConvKernel<<<dimGrid, dimBlock>>>(A_d, B_d, height, width);
 
-    // convolution has been performed; now copy result from B_d back to host memory
+            break;
+        }
+        case 1: {
+
+            float blockSize = INDIM;
+
+            dim3 dimBlock(blockSize, blockSize, 1);
+	    float test = width/(float)OUTDIM;
+	    //printf("%f\t%f\n", test, blockSize);
+            dim3 dimGrid(ceil(width/(float)OUTDIM), ceil(height/(float)OUTDIM), 1);
+
+            tiledHaloConvKernelT2In<<<dimGrid, dimBlock>>>(A_d, B_d, height, width);
+
+            break;
+        }
+    }
+
+    // convolution has been performed; now copy result from B_d (GPU) back to host memory
     cudaMemcpy(B_h, B_d, size, cudaMemcpyDeviceToHost);
     
     // deallocate GPU memory
@@ -112,7 +181,7 @@ int main() {
 
     //printf("%zu\n", sizeof(F_c)); // confirming F_c visibility and size
 
-    doConv(A, B, F, height, width);
+    doConv(A, B, F, height, width, 1);
 
     //for (int i=0; i<width*height; ++i) {
     //    printf("%f\t%f\n", A[i], B[i]);
@@ -121,9 +190,9 @@ int main() {
     for (int y=0; y<height; ++y) {
         for (int x=0; x<width; ++x)
             printf("%f\t", B[y*width + x]);
-	printf("\n");
+        printf("\n");
     }
-    
+
     // memory deallocation
     free(A);
     free(B);
